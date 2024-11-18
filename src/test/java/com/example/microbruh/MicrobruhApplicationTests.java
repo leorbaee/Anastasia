96da8bde9aa1dcac3ad1b739325c82bfd1227dd4
leorbaee <amessershmi@mail.ru>
2024-11-18 12:38:54 +0600

    init

---
+++ src/test/java/com/example/microbruh/MicrobruhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microbruh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobruhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
